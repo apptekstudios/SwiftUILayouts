fa8e96966c80f0147fa50f4877dd4fd66948fa19
tbren <contact@apptekstudios.com>
2022-06-11 18:15:55 +1000

    Initial Commit

---
+++ SwiftUILayouts/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUILayouts
+//
+//  Created by T Brennan on 11/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUILayouts/SwiftUILayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILayoutsApp.swift
+//  SwiftUILayouts
+//
+//  Created by T Brennan on 11/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
